8267aa2d72011939d91be5c5b30d0a15dd3c1336
Stiflerzak <hilarymwendia97@gmail.com>
2023-01-14 14:27:18 +0300

    Added my projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,21 @@
 import React from "react";
+import user from "../data/user";
+
 
 function ProjectItem({ name, about, technologies }) {
+
+ const technologyList= technologies.map((technology) => {
+return <li>{technology}</li>
+  })
+console.log({technologyList});
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {<span>{technologies}</span>}
+        
+        <span>{technologyList}</span>
       </div>
     </div>
   );
--- src/data/user.js
+++ src/data/user.js
@@ -1,10 +1,10 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Hilary",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
+    github: "https://github.com/stiflerzak",
     linkedin: "https://www.linkedin.com/in/liza/",
   },
   projects: [
